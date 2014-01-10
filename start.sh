2a31440b31acae8b93196ad13ea3d36755eedc07
zhaoxin <zhaoxina@gmail.com>
2014-01-10 03:16:41 -0800

    add heart to start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 cd `dirname $0`
 NAME="shorty"
+export HEART_COMMAND="erl -heart"
 screen -dmS $NAME -t $NAME \
-    erl -name "${NAME}@127.0.0.1" -pa $PWD/ebin $PWD/deps/*/ebin -s $NAME -s reloader \
+    erl -heart -name "${NAME}@127.0.0.1" -pa $PWD/ebin $PWD/deps/*/ebin -s $NAME -s reloader \
         +K         true \
         +A         128 \
         -env       ERL_MAX_PORTS 64000 \

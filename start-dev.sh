a98fdace4734776e70105991639cc9f6c67cd55f
zhaoxin <zhaoxina@gmail.com>
2014-01-14 03:02:44 -0800

    modified HEARTCOMMAND

--- start-dev.sh
+++ start-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 NAME="shorty"
 cd `dirname $0`
-erl -name "${NAME}@127.0.0.1" -pa $PWD/ebin $PWD/deps/*/ebin -s $NAME -s reloader \
+erl -name ${NAME}@127.0.0.1 -pa $PWD/ebin $PWD/deps/*/ebin -s $NAME -s reloader \
     +K         true \
     +A         128 \
     -env       ERL_MAX_PORTS 64000 \
--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 cd `dirname $0`
 NAME="shorty"
-export HEART_COMMAND="erl -heart"
-screen -dmS $NAME -t $NAME \
-    erl -heart -name "${NAME}@127.0.0.1" -pa $PWD/ebin $PWD/deps/*/ebin -s $NAME -s reloader \
+SHORTY_COMMAND="erl -heart -name ${NAME}@127.0.0.1 -pa ${PWD}/ebin ${PWD}/deps/*/ebin -s ${NAME} -s reloader \
         +K         true \
         +A         128 \
         -env       ERL_MAX_PORTS 64000 \
         -env       ERL_FULLSWEEP_AFTER 0 \
         -smp       enable \
         +zdbbl     32768 \
-        -setcookie $NAME
+        -setcookie ${NAME}"
+
+export HEART_COMMAND="${SHORTY_COMMAND}"
+screen -dmS $NAME -t $NAME ${SHORTY_COMMAND}

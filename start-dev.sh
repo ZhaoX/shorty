548274298c81184480755b557297687caad8dacc
zhaoxin <zhaoxina@gmail.com>
2014-07-01 11:13:50 +0800

    close mongodb connection clearly

--- start-dev.sh
+++ start-dev.sh
@@ -3,7 +3,7 @@ NAME="shorty"
 cd `dirname $0`
 erl -name ${NAME}@127.0.0.1 -pa $PWD/ebin $PWD/deps/*/ebin -s $NAME -s reloader \
     +K         true \
-    +A         128 \
+#    +A         32 \
     -env       ERL_MAX_PORTS 64000 \
     -env       ERL_FULLSWEEP_AFTER 0 \
     -smp       enable \
